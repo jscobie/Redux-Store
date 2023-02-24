bd0b216831ab9f4f73437f94b9b13aa6eea5f128
Jacob Scobie <jscobie@focus-solutions.net>
2023-02-23 21:09:46 -0500

    adding redux to pages

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,31 +11,32 @@ import {
   TOGGLE_CART,
 } from './actions';
 
-// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+const initalState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+}
+
+export default function reducer(state = initalState, action) {
   switch (action.type) {
-    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
-    // Your comment here
     case UPDATE_PRODUCTS:
       return {
         ...state,
         products: [...action.products],
       };
-
     case ADD_TO_CART:
       return {
         ...state,
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
-
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
-    // Your comment here
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
@@ -47,52 +48,37 @@ export const reducer = (state, action) => {
           return product;
         }),
       };
-
-    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
-    // Your comment here
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
         return product._id !== action._id;
       });
-
       return {
         ...state,
         cartOpen: newState.length > 0,
         cart: newState,
       };
-
     case CLEAR_CART:
       return {
         ...state,
         cartOpen: false,
         cart: [],
       };
-
     case TOGGLE_CART:
       return {
         ...state,
         cartOpen: !state.cartOpen,
       };
-
     case UPDATE_CATEGORIES:
       return {
         ...state,
         categories: [...action.categories],
       };
-
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
         currentCategory: action.currentCategory,
       };
-
-    // TODO: Add a comment describing what the default case is for
-    // Your comment here
     default:
       return state;
   }
 };
-
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
